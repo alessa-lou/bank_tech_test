db91ed2e6bce005c0e6389966effcaca95597f44
Alessa <alessa_hardwick@live.co.uk>
2020-08-11 13:14:15 +0100

    fixed rubocop errors

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,37 +1,31 @@
 class BankAccount
-
   attr_accessor :balance, :credit, :debit, :date, :bank_statement
 
   def initialize
-
     @balance = 0
     @bank_statement = []
-    @credit = ""
-    @debit = ""
-    @date = Time.now.strftime("%d/%m/%Y")
+    @credit = ''
+    @debit = ''
+    @date = Time.now.strftime('%d/%m/%Y')
   end
 
   def deposit(amount)
     @balance += amount
     @debit = amount
-    @bank_statement << {date: @date, credit: @credit, debit: @debit, balance: @balance}
+    @bank_statement << { date: @date, credit: @credit, debit: @debit, balance: @balance }
   end
 
   def withdraw(amount)
-    if
-      @balance <= 0
-      raise "You have no funds left"
-    else
-      @balance -= amount
-      @credit = amount
-      @bank_statement << {date: @date, credit: @credit, debit: @debit, balance: @balance}
-    end
+    raise 'You have no funds left' if @balance <= 0
+    
+    @balance -= amount
+    @credit = amount
+    @bank_statement << { date: @date, credit: @credit, debit: @debit, balance: @balance }
   end
 
   def print_statement
-
-     @bank_statement.collect do |transaction|
-      "date  ||  credit  ||  debit  ||  balance  \n #{transaction.values.join("  ||  ")}"
-     end
+    @bank_statement.collect do |transaction|
+      "date  ||  credit  ||  debit  ||  balance  \n #{transaction.values.join('  ||  ')}"
+    end
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,24 +1,22 @@
 require 'bank_account'
 
 describe BankAccount do
+  subject(:account) { BankAccount.new }
   it 'initializes a new bank account instance' do
     expect(BankAccount.new).to be_an_instance_of(BankAccount)
   end
 
   it 'initializes a new bank account instance with an empty balance' do
-    account = BankAccount.new
     expect(account.balance).to eq(0)
   end
 
   describe '#deposit' do
     it 'user can deposit an amount into their bank account' do
-      account = BankAccount.new
       account.deposit(100.00)
       expect(account.balance).to eq(100.00)
     end
 
     it 'user can deposit more money and the total balance increases' do
-      account = BankAccount.new
       account.deposit(100.00)
       account. deposit(100.00)
       expect(account.balance).to eq(200.00)
@@ -27,53 +25,45 @@ describe BankAccount do
 
   describe '#withdraw' do
     it 'user can withdraw money from their account' do
-      account = BankAccount.new
       account.deposit(100.00)
       account.withdraw(50.00)
       expect(account.balance).to eq(50.00)
     end
 
     it 'user cannot withdraw more money than they have' do
-      account = BankAccount.new
       account.deposit(100.00)
       account.withdraw(100.00)
-      expect{account.withdraw(10.00)}.to raise_error("You have no funds left")
+      expect { account.withdraw(10.00) }.to raise_error('You have no funds left')
     end
   end
 
   describe '#print_statement' do
     it 'user can see their balance on the bank statement' do
-      account = BankAccount.new
       account.deposit(100.00)
       expect(account.print_statement[0]).to include("#{account.bank_statement[0][:balance]}")
     end
 
     it 'user can see money depositing transactions' do
-      account = BankAccount.new
       account.deposit(100.00)
       expect(account.print_statement[0]).to include("#{account.bank_statement[0][:debit]}")
     end
 
     it 'user can see money withdrawal transactions' do
-      account = BankAccount.new
       account.deposit(100.00)
       account.withdraw(50.00)
       expect(account.print_statement[0]).to include("#{account.bank_statement[0][:credit]}")
     end
 
     it 'user can see the date on each transaction' do
-      account = BankAccount.new
       account.deposit(100.00)
       account.withdraw(50.00)
       expect(account.print_statement[0]).to include("#{account.bank_statement[0][:date]}")
     end
 
     it 'user can see the transactions in a formatted table' do
-      account = BankAccount.new
       account.deposit(100.00)
       account.withdraw(50.00)
       expect(account.print_statement[1]).to eq("date  ||  credit  ||  debit  ||  balance  \n #{account.bank_statement[1][:date]}  ||  #{account.bank_statement[1][:credit]}  ||  #{account.bank_statement[1][:debit]}  ||  #{account.bank_statement[1][:balance]}")
     end
   end
-
 end
